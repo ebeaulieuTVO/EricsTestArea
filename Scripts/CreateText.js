b70a246159c163ff6934b6c38af9df18dc214035
Eric Beaulieu <ebeaulieu@tvo.org>
2026-07-16 09:48:02 -0400

    Code Fix

--- Scripts/CreateText.js
+++ Scripts/CreateText.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
 
+fs.mkdirSync("./output", { recursive: true });
+
+fs.writeFileSync("./output/hello.txt", "Hello from GitHub Actions!");
+
 const content = `Hello from GitHub Actions
 Created at: ${new Date().toISOString()}/n
 Testing the workflow/n Eric was here/n Hello from GitHub Actions/n
 `;
 
-fs.writeFileSync("output.txt", content);
+fs.writeFileSync("./output/hello.txt", content);
 
-console.log("Created output.txt");
+console.log("Created output/hello.txt");
